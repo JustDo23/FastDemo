4b5768bf411fd32b705f103eda58962e87aef7d5
JustDo23 <757307903@qq.com>
2017-07-12 21:11:18 +0800

    Android Vector 中的 Path 学习

---
+++ app/src/main/java/com/just/fast/module/vector/VectorDrawActivity.java
@@ -0,0 +1,22 @@
+package com.just.fast.module.vector;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.just.fast.R;
+
+/**
+ * 学习了解 Vector 中的路径
+ *
+ * @author JustDo23
+ * @webUrl [http://www.cnblogs.com/yuhanghzsd/p/5466846.html]
+ * @since 2017年07月11日
+ */
+public class VectorDrawActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_vector_draw);
+  }
+}
