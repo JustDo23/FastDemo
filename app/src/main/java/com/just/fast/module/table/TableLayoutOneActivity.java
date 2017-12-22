e8e2724d07e6c851987b7449b0437293cd35aa4c
JustDo23 <757307903@qq.com>
2017-12-22 16:19:54 +0800

    表格布局测试

---
+++ app/src/main/java/com/just/fast/module/table/TableLayoutOneActivity.java
@@ -0,0 +1,22 @@
+package com.just.fast.module.table;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.just.fast.R;
+
+/**
+ * 表格布局
+ *
+ * @author JustDo23
+ * @webUrl [https://developer.android.com/guide/topics/ui/layout/grid.html#example]
+ * @since 2017年07月11日
+ */
+public class TableLayoutOneActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_table_layout_one);
+  }
+}
